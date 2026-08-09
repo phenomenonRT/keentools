4d88a2491c8b406e12559ae72f5854c737cd2572
phenomenonRT <erararda1234@gmail.com>
2026-08-09 13:06:54 +0300

    add

--- modules/AWG-Manager/install.sh
+++ modules/AWG-Manager/install.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 set -e
-data_dir="$KT_MODULE_DIR/data"
-mkdir -p "$data_dir"
-chmod +x "$KT_MODULE_DIR/worker.sh"
-echo "Демо-сервис установлен. Данные: $data_dir"
+opkg update && opkg upgrade
+wget -qO- http://repo.hoaxisr.ru/install.sh | sh
+echo "AWG Manager установлен."
 exit 0
